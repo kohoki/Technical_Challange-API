254de3429f18a7160ac31d9a07d4bf271cc3da97
Ralf Bonnick <ralfbonnick@gmx.net>
2023-01-11 16:57:32 +0100

    first route is done and works

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,9 +1,14 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
+const phoneData = require("../data/phones.json");
 
 /* GET home page */
 router.get("/", (req, res, next) => {
   res.render("index");
+});
+
+router.get("/phones", (req, res) => {
+  res.status(201).json({ phoneData });
 });
 
 module.exports = router;
